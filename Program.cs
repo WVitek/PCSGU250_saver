e6e29453b2b8f9cce1c86a5a736aa158ba5f3da5
WVitek <wvn@bk.ru>
2019-11-17 23:00:24 +0500

    Refactor, +save to text

--- Program.cs
+++ Program.cs
@@ -1,9 +1,9 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Diagnostics;
 using System.Drawing;
 using System.IO;
 using System.Linq;
+//using System.Linq;
 using System.Runtime.InteropServices;
 using System.Text;
 
@@ -33,91 +33,199 @@ namespace PCSGU250_saver
 
         class Channel
         {
-            int ch;
+            public int Ch { get; private set; }
+
             int[] dataBuf = new int[5000];
-            public Channel(int ch) { this.ch = ch; }
+            int[] prevCfg = new int[iBuf_SamplesBeg];
+            public string CfgStr { get; private set; }
+
+            public Channel(int ch) { this.Ch = ch; }
+
+            public bool FetchData()
+            {
+                for (int i = 0; i < prevCfg.Length; i++) prevCfg[i] = dataBuf[i];
+
+                switch (Ch)
+                {
+                    case 1: ReadCh1(ref dataBuf[0]); break;
+                    case 2: ReadCh2(ref dataBuf[0]); break;
+                    default: return false;
+                }
+
+                for (int i = iBuf_SamplesBeg; i < iBuf_SamplesEnd; i++)
+                    if (dataBuf[i] != 255)
+                        return true;
+                return false;
+            }
+
+            public void Stop()
+            {
+                for (int i = 0; i < prevCfg.Length; i++) prevCfg[i] = 0;
+                FileName = null;
+            }
+
+            public bool CfgChanged()
+            {
+                for (int i = 0; i < prevCfg.Length; i++)
+                    if (prevCfg[i] != dataBuf[i])
+                        return true;
+                return false;
+            }
+
+            static string MinLenStr(params string[] strs)
+            {
+                int iMin = 0;
+                for (int i = 1; i < strs.Length; i++)
+                    if (strs[iMin].Length > strs[i].Length)
+                        iMin = i;
+                return strs[iMin];
+            }
+
+            public void UpdateFileName(DateTime firstTime, string dir)
+            {
+                int F = dataBuf[iBuf_Freq_Hz];
+                int V = dataBuf[iBuf_Volt_mV];
+                int G = dataBuf[iBuf_GndLevel_ADCcounts];
+                var sF = MinLenStr($"{F}Hz", $"{F * 0.001}kHz", $"{F * 0.000001}MHz");
+                var sV = MinLenStr($"{V}mV", $"{F * 0.001}V");
+                CfgStr = (G > 0)
+                    ? $"#{Ch},F={sF},V={sV},nGND={G}"
+                    : $"#{Ch},F={sF},V={sV}";
+                FileName = Path.Combine(dir, $"{firstTime:yyyyMMdd_HHmm} {CfgStr}.tsv.txt");
+                firstRow = true;
+            }
+
+            public string FileName { get; private set; }
+            bool firstRow;
+
+            public void SaveDataAsText(DateTime dataTime)
+            {
+                var sb = new StringBuilder(4096 * 5);
+                sb.Append(dataTime.ToString("yyyy-MM-dd HH:mm:ss.fff"));
+                sb.Append('\t');
+                for (int i = iBuf_SamplesBeg; i < iBuf_SamplesEnd; i++)
+                { sb.Append('\t'); sb.Append(dataBuf[i]); }
+                sb.AppendLine();
+                try { File.AppendAllText(FileName, sb.ToString(), Encoding.ASCII); }
+                catch { Console.WriteLine($"#{Ch}: ERROR SAVING TO FILE: {FileName}"); }
+                firstRow = false;
+            }
+
+            public void PrintPixels(int w)
+            {
+                int d = nBuf_Samples / w;
+                if (d == 0) d = 1;
+                for (int j = 0; j < w; j++)
+                {
+                    int i0 = j * d;
+                    int i1 = i0 + d - 1;
+                    byte min = 255, max = 0;
+                    int sum = 0;
+                    for (int i = i0; i < i1; i++)
+                    {
+                        var b = (byte)dataBuf[iBuf_SamplesBeg + i];
+                        if (b < min) min = b;
+                        if (b > max) max = b;
+                        sum += b;
+                    }
+                    byte avg = (byte)(sum / d);
+                    PixelConsole.WriteHeat(sum / (255f * d));
+                }
+            }
         }
+
 
         static void Main(string[] args)
         {
-            Console.WriteLine("START saving data from PCSGU250.");
-            Console.WriteLine("Press [Esc] to stop and exit.");
+            Console.WriteLine("START receiving data from PCSGU250.");
+            const string HelpMsg = "Press [Esc] to stop and exit, [Space] to start/pause recording.";
+            Console.WriteLine(HelpMsg);
 
-            var dataBuf = new int[5000];
-            var bytes = new byte[nBuf_Samples];
-            int prevFreq = 0;
             var sw = new Stopwatch();
             int nCounts = 0;
             sw.Start();
             int prevPulse = -1;
+            bool REC = false;
+
+            var ch1 = new Channel(1); Directory.CreateDirectory("CH1");
+            var ch2 = new Channel(2); Directory.CreateDirectory("CH2");
 
             while (true)
             {
-                if (Console.KeyAvailable && Console.ReadKey().Key == ConsoleKey.Escape)
-                    break;
+                if (Console.KeyAvailable)
+                {
+                    var k = Console.ReadKey(true).Key;
+                    if (k == ConsoleKey.Escape)
+                        break;
+                    else if (k == ConsoleKey.Spacebar)
+                    {
+                        REC = !REC;
+                        Console.WriteLine(REC ? "Recording started!" : "Recording paused...");
+                        if (!REC) { ch1.Stop(); ch2.Stop(); }
+                    }
+                    else Console.WriteLine(HelpMsg);
+                }
                 if (DataReady())
                 {
-                    ReadCh1(ref dataBuf[0]);
-                    int currFreq = dataBuf[iBuf_Freq_Hz];
-                    if (prevFreq != currFreq)
+                    nCounts++;
+                    var time = DateTime.Now;
+                    var with1 = ch1.FetchData();
+                    var with2 = ch2.FetchData();
+                    bool changed = false;
+                    if (with1 && ch1.CfgChanged())
+                    { changed = true; ch1.UpdateFileName(time, "CH1"); }
+                    if (with2 && ch2.CfgChanged())
+                    { changed = true; ch2.UpdateFileName(time, "CH2"); }
+                    if (changed)
                     {
-                        sw.Stop(); sw.Start();
-                        // todo: flush data acquired at previos frequency
-                        Console.WriteLine($"Max Freq. = {currFreq} Hz");
-                        // todo: create new file
-                        nCounts = 0;
-                        prevFreq = currFreq;
-                    }
-                    bool withData = false;
-                    for (int i = iBuf_SamplesBeg; i < iBuf_SamplesEnd; i++)
-                    {
-                        if (dataBuf[i] < 255)
-                            withData = true;
-                        bytes[i - iBuf_SamplesBeg] = (byte)dataBuf[i];
-                    }
-                    if (withData)
-                    {
-                        nCounts++;
-                        using (var fs = new FileStream("data_log.bin", FileMode.Append, FileAccess.Write, FileShare.Read))
-                            fs.Write(bytes, 0, nBuf_Samples);
-                        int w = Console.BufferWidth - 1;
-                        int d = nBuf_Samples / w;
-                        if (d == 0) d = 1;
-                        for (int j = 0; j < w; j++)
+                        int left = "REC 00:00:00.000 ".Length;
+                        Console.CursorLeft = left;
+                        if (with1)
                         {
-                            int i0 = j * d;
-                            int i1 = i0 + d - 1;
-                            byte min = 255, max = 0;
-                            int sum = 0;
-                            for (int i = i0; i < i1; i++)
-                            {
-                                var b = bytes[i];
-                                if (b < min) min = b;
-                                if (b > max) max = b;
-                                sum += b;
-                            }
-                            byte avg = (byte)(sum / d);
-                            //PixelConsole.Write(Color.FromArgb(avg, min, max));
-                            PixelConsole.WriteHeat(sum / (255f * d));
+                            Console.Write(ch1.CfgStr);
+                            Console.CursorLeft = left + (Console.BufferWidth - 1 - left) / 2;
                         }
-                        Console.ResetColor();
+                        if (with2)
+                            Console.Write(ch2.CfgStr);
                         Console.WriteLine();
                     }
+                    if (REC)
+                    {
+                        Console.BackgroundColor = ConsoleColor.DarkRed;
+                        Console.ForegroundColor = ConsoleColor.Yellow;
+                    }
+                    Console.Write("REC");
+                    Console.ResetColor();
+                    Console.Write($" {time:HH:mm:ss.fff} ");
+                    int w = Console.BufferWidth - Console.CursorLeft - 1;
+                    if (with1 && with2)
+                        w /= 2;
+                    if (with1)
+                    {
+                        ch1.PrintPixels(w);
+                        Console.ResetColor();
+                        if (REC) ch1.SaveDataAsText(time);
+                    }
+                    if (with2)
+                    {
+                        ch2.PrintPixels(w);
+                        Console.ResetColor();
+                        if (REC) ch2.SaveDataAsText(time);
+                    }
+                    Console.WriteLine();
                 }
                 else System.Threading.Thread.Sleep(1);
 
-                int currPulse = (int)(sw.ElapsedMilliseconds >> 12);
+                int currPulse = (int)(sw.ElapsedMilliseconds >> 14);
                 if (currPulse != prevPulse)
                 {
                     if (nCounts == 0)
                         Console.WriteLine("Press [Run] in PCSGU250 GUI to start sampling...");
-                    //else
-                    //    Console.WriteLine($"Buffers readed: {nCounts}");
                     nCounts = 0;
                     prevPulse = currPulse;
                 }
             }
-            Console.WriteLine("STOP saving data.");
+            Console.WriteLine("STOP receiving data from PCSGU250.");
         }
     }
 
